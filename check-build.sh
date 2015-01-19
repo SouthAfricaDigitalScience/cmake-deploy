d0e7bd8bb4ff533365261611e03db7a53be10185
Bruce Becker <brucellino@gmail.com>
2015-01-19 15:48:53 +0100

    Added the check script

---
+++ check-build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+module load ci
+echo "checking $NAME"
+cd $WORKSPACE/$NAME-$VERSION
+make check
+
+echo $?
+
+make install # DESTDIR=$SOFT_DIR
+
+mkdir -p $REPO_DIR
+rm -rf $REPO_DIR/*
+tar -cvzf $REPO_DIR/build.tar.gz -C $WORKSPACE/build apprepo
+
+mkdir -p modules
+(
+cat <<MODULE_FILE
+#%Module1.0
+## $NAME modulefile
+##
+proc ModulesHelp { } {
+    puts stderr "       This module does nothing but alert the user"
+    puts stderr "       that the [module-info name] module is not available"
+}
+
+module-whatis   "$NAME $VERSION."
+setenv       CMAKE_VERSION       $VERSION
+setenv       CMAKE_DIR           /apprepo/$::env(SITE)/$::env(OS)/$::env(ARCH)/$NAME/$VERSION
+prepend-path PATH              $::env(CMAKE_DIR)/bin
+prepend-path LD_LIBRARY_PATH   $::env(CMAKE_DIR)/lib
+prepend-path GCC_INCLUDE_DIR   $::env(CMAKE_DIR)/include
+MODULE_FILE
+) > modules/$VERSION
+
+mkdir -p $LIBRARIES_MODULES/$NAME
+cp modules/$VERSION $LIBRARIES_MODULES/$NAME
