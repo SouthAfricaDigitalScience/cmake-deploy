223a1703878031772a358ccd34234c79b38f99c9
Bruce Becker <brucellino@gmail.com>
2017-06-07 20:01:54 +0200

    See https://cmake.org/Bug/view.php?id=10544

--- build.sh
+++ build.sh
@@ -63,7 +63,7 @@ tar xzf ${SRC_DIR}/${SOURCE_FILE} -C ${WORKSPACE} --skip-old-files
 echo "Going to ${WORKSPACE}/${NAME}-${VERSION}"
 cd ${WORKSPACE}/${NAME}-${VERSION}
 ls
-CXX=c++ MAKE=make ./bootstrap \
+OBJECT_MODE=64 CXX=c++ MAKE=make ./bootstrap \
 --prefix=${SOFT_DIR} \
 --no-qt-gui
 make
